270949b99b79ec5967ac2eb3e8f8af5e38b6e53e
Aayush Sapkota <sapkotaaayush708@gmail.com>
2025-08-27 16:55:24 +0545

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../../assets/footer_logo.svg'
+import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={footer_logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequatur culpa ut, doloremque soluta ratione.</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={user_icon} alt="" />
+                    <input type="email" placeholder='Enter your email'/>
+                </div>
+                <div className="footer-subscribe">Subscribe</div>
+            </div>
+        </div>
+
+        <hr />
+
+        <div className="footer-bottom">
+            <p className="footer-botton-left">&copy; 2025 Aayush Sapkota. All rights reserved.</p>
+            <div className="footer-bottom-right">
+                <p>Term or Services</p>
+                <p>Privacy Policy</p>
+                <p>Connect with me</p>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className="hero"> 
+      <img src={profile_img} alt="profile img" />
+      <h1><span>I'm Aayush sapkota,</span> Backend developer based in Nepal.</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, assumenda! Non consectetur vitae eius a.</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
